19466153cd8d5c9135d2a87f4ed19fa4fbb67456
남아현 <ska6371@gmail.com>
2021-07-03 23:24:08 +0900

    if문 변환

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,15 +2,7 @@ import React from 'react';
 
 function App() {
   const name = '리액트';
-  return (
-    <>
-    {name === '리액트' ? (
-      <h1>{name} 안녕</h1>
-    ) : (
-      <h2>리액트가 아닙니다.</h2>
-    )}
-    </>
-  );
+  return <div>{name == '리액트' && <h1>리액트입니다.</h1>}</div>
 }
 
 export default App;
