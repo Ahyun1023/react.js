af64acf655ae5e3c6dd5a0a0592aef2ce0219b29
남아현 <ska6371@gmail.com>
2021-07-04 21:12:08 +0900

    jsx에서는 undefined 렌더링 가능

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function App() {
+/*function App() {
   const name = '리액트';
   return <div>{name == '리액트' && <h1>리액트입니다.</h1>}</div>
+}*/
+
+function App(){
+  const name = undefined;
+  /*retur name;*/
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
