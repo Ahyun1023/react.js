71d10ae1e03bec097e4af7064387d4168fcb302e
남아현 <ska6371@gmail.com>
2021-07-05 21:57:05 +0900

    인라인 스타일링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -6,9 +6,15 @@ import React from 'react';
 }*/
 
 function App(){
-  const name = undefined;
-  /*retur name;*/
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16
+  };
+  return <div style={style}>{name}</div>
 }
 
 export default App;
