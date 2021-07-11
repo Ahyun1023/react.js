b66e51a130b28a5d6a1ebf64d643662dba0e7ed0
남아현 <ska6371@gmail.com>
2021-07-11 22:42:34 +0900

    props

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,8 +4,12 @@ import MyComponent from './MyComponent';
 import './App.css'
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent name="React" />
 }
+
+/*const App = () => {
+  return <MyComponent />;
+}*/
 
 /*class App extends Component{
   render(){
@@ -17,29 +21,6 @@ const App = () => {
 /*function App() {
   const name = '리액트';
   return <div>{name == '리액트' && <h1>리액트입니다.</h1>}</div>
-}*/
-
-/*function App(){
-  const name = '리액트';
-  const style = {
-    backgroundColor: 'black',
-    color:'aqua',
-    fontSize: '48px',
-    fontWeight: 'bold',
-    padding: 16
-  };
-  return <div style={style}>{name}</div>
-}*/
-
-/*function App(){
-  const name = 'react';
-  return(
-    <>
-      <div class="react">{name}</div>
-      <input />
-    </>
-  )
-
 }*/
 
 export default App;
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const MyComponent = () => {
-    return <div>나의...멋진 컴포넌트 ^.^</div>
+const MyComponent = props => {
+    return <div>아녕하세요, 제 이름은 {props.name}입니다.</div>
+}
+
+MyComponent.defaultProps = {
+    name: '기본 이름'
 }
 
 export default MyComponent;
