f613d47da7b6e26c1557e6dd628e4f8c4ff2e802
남아현 <ska6371@gmail.com>
2021-07-12 19:51:06 +0900

    비구조화 할당 문법, prop검증

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import PropsTypes from 'prop-types';
 
-const MyComponent = props => {
-    return (
+const MyComponent = ({name, children}) => {
+    
+    /*return (
         <div>
             안녕하세요, 제 이름은 {props.name}입니다. <br/>
             children 값은 {props.children}입니다.
         </div>
+    )*/
+
+    return (
+        <div>
+            안녕하세요, 제 이름은 {name}입니다. <br/>
+            children 값은 {children}입니다.
+        </div>
     )
-}
+};
 
 MyComponent.defaultProps = {
     name: '기본 이름'
-}
+};
+
+//string 형태의 props만 전달할 수 있음
+MyComponent.PropsTypes = {
+    name: PropsTypes.string
+};
 
 export default MyComponent;
