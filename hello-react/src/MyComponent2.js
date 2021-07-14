b9a8d71d64f701eed82e61b0b0d58a9009d74635
남아현 <ska6371@gmail.com>
2021-07-14 21:32:50 +0900

    클래스형 컴포넌트 props

---
+++ hello-react/src/MyComponent2.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropsTypes from 'prop-types';
+
+class MyComponent extends Component {
+    render() {
+        const {name, favoriteNumber, children} = this.props; // 비구조화 할당
+        return (
+            <div>
+                안녕하세요, 제 이름은 {name}입니다. <br/>
+                children값은 {children}입니다. <br/>
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        )
+    }
+}
+
+MyComponent.defaultProps = {
+    name: '기본 이름'
+};
+
+//string 형태의 props만 전달할 수 있음
+MyComponent.PropsTypes = {
+    name: PropsTypes.string,
+    favoriteNumber: PropsTypes.number.isRequired
+};
+
+export default MyComponent;
