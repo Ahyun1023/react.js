7eaece591a3c56c5b57bf6f14e4a7bb2c83c8955
남아현 <ska6371@gmail.com>
2021-07-19 22:17:53 +0900

    객체 대신 함수 인자 전달

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -17,7 +17,7 @@ class Counter extends Component {
 
     render(){
         const {number, fixNumber} = this.state;
-        return (
+        /*return (
             <div>
                 <h1>{number}</h1>
                 <h2>바뀌지 않는 값: {fixNumber}</h2>
@@ -26,12 +26,38 @@ class Counter extends Component {
                 onClick={() => {
                     //this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
                     this.setState({number: number + 1});
+                    this.setState({ number: this.state.number + 1 });
                 }}
                 >
                     +1
                 </button>
             </div>
-        );
+        );*/
+        return(
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixNumber}</h2>
+                <button
+                //onClick을 통해 버튼이 클릭되었을 때 호출할 함수 지정
+                onClick={() => {
+                    this.setState(prevState =>{
+                        return {
+                            number: prevState.number + 1
+                        };
+                    });
+
+                    //위 코드와 아래 코드는 완전히 같은 기능을 수행
+                    //아래 코드는 함수에서 바로 객체를 반환
+
+                    /*this.setState(prevState => {
+                        number: prevState.number + 1
+                    });*/
+                }}
+                >
+                    +1
+                </button>
+            </div>
+        )
     }
 }
 
